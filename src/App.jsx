51c9a38d7444e56a5aea1760254483a43bc832fa
19350308 <isc19350308@gmail.com>
2023-03-07 09:56:53 -0600

    Calculadora

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+    return (
+      <div>
+      <h1>Calculator</h1>
+      <table style={{width: '400px'}}>
+        <tr>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+        </tr>
+        <tr>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+        </tr>
+        <tr>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+        </tr>
+        <tr>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+        </tr>
+        <tr>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+        </tr>
+        <tr>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+        </tr>
+      </table>
+      </div>
+   
+    )
+  }
+  
+  export default App
+
