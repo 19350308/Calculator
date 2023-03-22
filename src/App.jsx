58b332da95f24c5104f7f0b1a1a7669dfff792f2
19350308 <isc19350308@gmail.com>
2023-03-22 09:40:50 -0600

    Añadiendo funcion de suma P1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import './App.css'
 const buttonsClasses= 'btn btn-primary w-75';
 
 function App() {
-  const [screen, setScreen] = useState('0')
+  const [number1, setNumber1] = useState('');
+  const [clearScreen, SetClearScreen] = useState(false);
+  const [screen, setScreen] = useState('0');
   
   const handleButtonClick = (e) => {
     const value = e.target.value;
@@ -12,7 +14,15 @@ function App() {
       if(screen.includes('.')) return;
     }
     if (value === 'C'){
-      setScreen(0);
+      setScreen('0');
+      return;
+    }
+    /// 
+    console.log({number1});
+    if (clearScreen) {
+      console.log('Cambia screen');
+      setScreen(value);
+      SetClearScreen(false);
       return;
     }
     if (screen ==='0' && value !== '.'){
@@ -30,6 +40,19 @@ function App() {
     setScreen(screen.slice(0, -1));
   }
 
+  const handleOperationButtonClick = (e) => {
+    const operator = e.target.value;
+    switch (operator) {
+      case '+':
+        setNumber1(screen);
+        console.log('Suma');
+        break;
+
+      default:
+        break;
+    }
+    SetClearScreen(true);
+  }
   return (
     <div className='app'>
     <h1>Calculator</h1>
@@ -93,7 +116,10 @@ function App() {
         <td rowSpan={2}><button
         type= "button"
          className={buttonsClasses}
-         style={{height: "80px"}}> + </button>
+         style={{height: "80px"}}
+         value='+'
+         onClick= {(e) => handleOperationButtonClick(e)}
+         > + </button>
           </td>
       </tr>
      {/*Fourth row */}
